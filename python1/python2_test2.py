04df8a9b4872583f8f135d584ec95cb5d0c8eb36
Sara Pini <sara.pini@unimore.it>
2025-10-14 15:39:31 -0400

    updating python2_test2.py

---
+++ python1/python2_test2.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+import sys
+
+test_number = int(sys.argv[1])
+
+print(test_number, 'is the tested number')
+if test_number > 0:
+    print('it is positive')
+    if test_number <= 50:
+        print('but smaller than or equal 50')
+        if test_number % 2 == 0:
+            print('it is an even number that is smaller than or equal 50')
+    elif test_number % 3 == 0:
+            print('it is larger than 50 and dvisible by 3')
+elif test_number < 0:
+    print('it is negative')
+else:
+    print('it must be 0')
