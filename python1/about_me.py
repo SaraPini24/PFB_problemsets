ffeb2c42e0f2d02de8c6ce2ba76c06db00c7b7dc
Sara Pini <sara.pini@unimore.it>
2025-10-14 11:36:58 -0400

    updating aboout_me.py

---
+++ python1/about_me.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import sys
+my_name = sys.argv[1]
+my_fav_color = sys.argv[2]
+my_fav_activity = sys.argv[3]
+my_fav_animal = sys.argv[4]
+
+print("My name:", my_name)
+print("My favortie color:", my_fav_color)
+print("My favorite activity:", my_fav_activity)
+print("My favorite animal:", my_fav_animal)
+
+print(f"My name: {my_name} \nMy favorite color: {my_fav_color} \nMy favorite activity: {my_fav_activity} \nMy favorite animal: {my_fav_animal}")
