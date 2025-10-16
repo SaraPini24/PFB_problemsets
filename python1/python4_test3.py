d912d7ebc6d581057e5feb8f05aa00dcfee6ec67
Sara Pini <sara.pini@unimore.it>
2025-10-15 20:28:11 -0400

    Updating python4_test3.py

--- python1/python4_test3.py
+++ python1/python4_test3.py
@@ -5,5 +5,26 @@
 import sys
 min = int(sys.argv[1])
 max = int(sys.argv[2])
+
+for i in range(min, max+1):
+    print(i)
+
+#Exercise 11
+import sys
+min = int(sys.argv[1])
+max = int(sys.argv[2])
 my_list = list(range(min,max+1))
-print(my_list)
+
+for i in my_list:
+    print(i)
+
+#Exercise 12
+import sys
+min = int(sys.argv[1])
+max = int(sys.argv[2])
+
+my_list1 =[]
+for i in range(min, max+1):
+    if int(i)%2 != 0:
+        my_list1.append(i)
+print(my_list1)
