3729519be71864e11b2a2ee45f58e24700e89591
Sara Pini <sara.pini@unimore.it>
2025-10-16 21:27:32 -0400

    Updating python4_test_bonus1.py

--- python1/python4_test_bonus1.py
+++ python1/python4_test_bonus1.py
@@ -2,5 +2,17 @@
 
 ###BONUS EXERCISE.2###
 
+dictionary={}
 with open("result.fasta_aln.txt") as my_seq: #Results of sequence alignment from Python_04.fasta.txt
+    for line in my_seq:
+        line =line.rstrip()
+        if '>' in line:
+            line_key=line.lstrip(">")
+            dictionary[line_key]=''
+        else:
+            dictionary[line_key]=dictionary[line_key]+line
+
+
+#for i in range(0,lungheza valori dizionario)):
     
+#print(dictionary)
